216d4324cb38c503ab47e0745c9df85917512965
Osbaldo Martinez <osbaldoiniguez@gmail.com>
2020-05-08 13:49:36 -0700

    Starter code for iOS app.

---
+++ CSC_690_Final/CSC_690_Final/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CSC_690_Final
+//
+//  Created by Osbaldo Martinez on 5/8/20.
+//  Copyright © 2020 Osbaldo Martinez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CSC_690_Final/CSC_690_FinalTests/CSC_690_FinalTests.swift
@@ -0,0 +1,34 @@
+//
+//  CSC_690_FinalTests.swift
+//  CSC_690_FinalTests
+//
+//  Created by Osbaldo Martinez on 5/8/20.
+//  Copyright © 2020 Osbaldo Martinez. All rights reserved.
+//
+
+import XCTest
+@testable import CSC_690_Final
+
+class CSC_690_FinalTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
